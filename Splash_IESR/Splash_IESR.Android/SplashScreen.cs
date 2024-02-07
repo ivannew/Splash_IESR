913d3f18f25f9a3d0f47475587b666b6fc758080
ivannew <ivaneriquesotorodriguez@gmail.com>
2024-02-06 19:32:49 -0700

    Agregar archivos de proyecto.

---
+++ Splash_IESR/Splash_IESR.Android/SplashScreen.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Splash_IESR.Droid
+{
+    [Activity(Label = "Splash_IESR", Icon = "@mipmap/icon", Theme = "@style/nuevoTema", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize)]
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+
+            // Create your application here
+        }
+    }
+}
